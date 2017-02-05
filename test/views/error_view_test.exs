51aaab91dccbceef9febe73b9f7561a86727a80c
Sammy Nave <sammy.nave@gmail.com>
2017-02-05 01:58:08 -0500

    fixup tests

--- test/views/error_view_test.exs
+++ test/views/error_view_test.exs
@@ -6,16 +6,16 @@ defmodule Api.ErrorViewTest do
 
   test "renders 404.json" do
     assert render(Api.ErrorView, "404.json", []) ==
-           %{errors: %{detail: "Page not found"}}
+           %{"errors" => [%{code: 404, title: "Not Found"}], "jsonapi" => %{"version" => "1.0"}}
   end
 
   test "render 500.json" do
     assert render(Api.ErrorView, "500.json", []) ==
-           %{errors: %{detail: "Internal server error"}}
+           %{"errors" => [%{code: 500, title: "Internal Server Error"}], "jsonapi" => %{"version" => "1.0"}}
   end
 
   test "render any other" do
     assert render(Api.ErrorView, "505.json", []) ==
-           %{errors: %{detail: "Internal server error"}}
+           %{"errors" => [%{code: 500, title: "Internal Server Error"}], "jsonapi" => %{"version" => "1.0"}}
   end
 end
