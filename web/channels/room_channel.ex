c0d93a1bdce39934d42b8404d4c89e27e7c8edc5
Sammy Nave <sammy.nave@gmail.com>
2017-02-18 13:44:40 -0500

    auth working and broadasting who enters based on current user

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,16 +1,23 @@
 defmodule Api.RoomChannel do
   use Phoenix.Channel
   require Logger
+  import Guardian.Phoenix.Socket
 
   def join("rooms:" <> room_id, message, socket) do
-    Process.flag(:trap_exit, true)
-    send(self, {:after_join, message})
+    user = Guardian.Phoenix.Socket.current_resource(socket)
 
-    {:ok, socket}
+    Process.flag(:trap_exit, true)
+    send(self, {:after_join, %{email: user.email} })
+
+    if user do
+      {:ok, "Hello #{user.email}! Joined Room:#{room_id}", socket}
+    else
+      {:error, %{reason: "unauthorized"}}
+    end
   end
 
-  def handle_info({:after_join, msg}, socket) do
-    broadcast! socket, "user:entered", %{user: msg["user"]}
+  def handle_info({:after_join, message}, socket) do
+    broadcast! socket, "user:entered", %{user: message.email}
     push socket, "join", %{status: "connected"}
     {:noreply, socket}
   end
