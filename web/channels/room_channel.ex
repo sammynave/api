9885e88e80b16cef0b22ec62bc7e23dbfa9b6125
Sammy Nave <sammy.nave@gmail.com>
2017-02-18 14:09:26 -0500

    auth sockets working and not sending duplicate messages to author

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -2,6 +2,7 @@ defmodule Api.RoomChannel do
   use Phoenix.Channel
   require Logger
   import Guardian.Phoenix.Socket
+  intercept ["new:message"]
 
   def join("rooms:" <> room_id, message, socket) do
     user = Guardian.Phoenix.Socket.current_resource(socket)
@@ -25,5 +26,14 @@ defmodule Api.RoomChannel do
   def terminate(reason, _socket) do
     Logger.debug"> leave #{inspect reason}"
     :ok
+  end
+
+  def handle_out("new:message", payload, socket) do
+    user = Guardian.Phoenix.Socket.current_resource(socket)
+
+    if payload.owner_id != user.id do
+      push socket, "new:message", payload.message
+    end
+    {:noreply, socket}
   end
 end
